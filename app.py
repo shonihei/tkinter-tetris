ae8eeeade79e41bf2e42b0ff7219610ae0e66416
shonihei <shonihei@gmail.com>
2017-02-18 16:03:02 -0500

    Makes a board and works on initialization of the game

--- app.py
+++ app.py
@@ -1,10 +1,52 @@
-from tkinter import *
+import tkinter as tk
+from board import *
+import random as R
 
-master = Tk()
+class app(tk.Frame):
+    pieces = ["O","I","S","Z","L","J","T"]
 
-w = Canvas(master, width=200, height=100)
-w.pack()
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.parent.title("Hello")
+        self.tiles = {}
+        self.gameStarted = False
+        self.boardState = Board(20, 10, 30)
+        self.parent.bind("<KeyPress>", self.keydown)
+        self.c = tk.Canvas(self.parent, width=400, height=600)
+        self.drawBoard(self.c)
+        self.c.pack()
 
-w.create_rectangle(50, 25, 150, 75, fill="blue")
+    def keydown(self, e):
+        # space = 32
+        # UP = 63232
+        # DOWN = 63233
+        # LEFT = 63234
+        # RIGHT = 63235
 
-mainloop()
+        # Start game
+        if ord(e.char) == 32:
+            if not self.gameStarted:
+                piece_index = R.randint(0, 6)
+                self.boardState.add_piece(app.pieces[piece_index])
+                self.gameStarted = True
+                self.drawBoard(self.c)
+        else:
+            pass
+
+    def drawBoard(self, canvas):
+        size = self.boardState.square_width
+        for r in range(self.boardState.rows):
+            for c in range(self.boardState.cols):
+                x1 = c * size
+                y1 = r * size
+                x2 = x1 + size
+                y2 = y1 + size
+                piece = self.boardState.piece_at(r, c)
+                tile = canvas.create_rectangle(x1,y1,x2,y2, fill=Board.colors[piece])
+                self.tiles[r,c] = tile
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app(root).pack()
+    root.mainloop()
--- board.py
+++ board.py
@@ -9,31 +9,46 @@ class Board:
         "T": [(-1, 0), (0, -1,), (0, 0), (1, 0)]
     }
 
+    colors = {
+        " ": "grey",
+        "O": "yellow",
+        "I": "cyan",
+        "S": "red",
+        "Z": "green",
+        "L": "orange",
+        "J": "pink",
+        "T": "purple"
+    }
+
     def __init__(self, numRows, numCols, sqwidth):
-        """
-            First four rows act as a buffer and is not meant to be rendered
-        """
-        self.board = [[0 for i in range(numCols)] for j in range(numRows)]
+        self.board = [[" " for i in range(numCols)] for j in range(numRows)]
         self.rows = numRows
         self.cols = numCols
+        self.centerCol = self.cols // 2 - 1
         self.square_width = sqwidth
         self.__bufferEmpty = True
 
     def __repr__(self):
         s = ""
         for i in range(self.rows):
+            s += "|"
             for j in range(self.cols):
-                s += str(self.board[i][j])
+                s += str(self.board[i][j]) + "|"
             s += "\n"
         return s
 
     def add_piece(self, piece):
         piece_schematic = Board.pieces[piece]
-        centerCol = self.cols // 2
         for coordinate in piece_schematic:
-            self.board[0][centerCol]
+            self.board[0-coordinate[1]][self.centerCol-coordinate[0]] = piece
+
+    def piece_at(self, row, col):
+        return self.board[row][col]
+
 def main():
     b = Board(20, 10, 10)
+    #print(b)
+    b.add_piece("O")
     print(b)
 
 if __name__ == "__main__":
